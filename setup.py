85e2bc21973d31e191127732e923eca72f6ed6cb
Mikey Garcia <gikeymarcia@gmail.com>
2021-09-29 15:18:23 -0700

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (Path(__file__).parent / "README.md").read_text()
 # https://setuptools.readthedocs.io/en/latest/setuptools.html#basic-use
 setup(
     name="mydot",
-    version="0.4.0",
+    version="0.4.1",
     description="Manage and edit $HOME dotfiles using Python + git = <3",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -16,5 +16,5 @@ setup(
     author_email="gikeymarcia@gmail.com",
     license="GPL-3.0",
     packages=find_packages(exclude="tests"),
-    install_requires=["pydymenu>=0.4.0", "rich"],
+    install_requires=["pydymenu>=0.4.1", "rich"],
 )
